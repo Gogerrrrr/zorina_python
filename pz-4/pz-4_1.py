591cc43cf7ee2e8d43970c9c3a22e593ab9f9d65
Gogerrrrr <zorinadarya682@gmail.com>
2024-11-14 23:31:01 +0300

    pz-4

---
+++ pz-4/pz-4_1.py
@@ -0,0 +1,11 @@
+#дано целое число N (>0). Найти сумму 1 + 1/2 + 1/3...+ 1/N
+while True:
+    try:#обработка исключений
+        n = int(input("Введите число N:"))
+        sum = 0
+        for i in range(1, n + 1):
+            sum = 1 / i
+            print("Сумма равна:", sum)
+        break
+    except ValueError:#возвращение на try
+        print("Введите корректное число!")
