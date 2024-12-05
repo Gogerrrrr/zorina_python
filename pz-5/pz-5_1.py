0ff358160f44a1463cd35e841d8bd2b12f4e1a3a
Gogerrrrr <zorinadarya682@gmail.com>
2024-12-05 23:37:27 +0300

    pz-5_1.py

--- pz-5/pz-5_1.py
+++ pz-5/pz-5_1.py
@@ -1,8 +1,8 @@
 
 import random
 
-def generate_number():
-    try:
+def generate_number(): #вызов функции, генерация чисел
+    try: #обработка исключений
         number = random.randint(1000, 9999)
         if chk_dubls(number):
             print(f"Число {number} содержит одинаковые числа")
@@ -11,6 +11,6 @@ def generate_number():
     except ValueError:
         print("Произошла ошибка")
 
-def chk_dubls(number):
+def chk_dubls(number): #вызов функции проверка на дубликаты чисел
     return len(set(str(number))) != 4
 print(generate_number())
