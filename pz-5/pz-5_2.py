a4f8564beab855c0b24c712b1499533ca45b2de3
Gogerrrrr <zorinadarya682@gmail.com>
2024-12-05 22:46:11 +0300

    pz-5_2.py

--- pz-5/pz-5_2.py
+++ pz-5/pz-5_2.py
@@ -5,12 +5,12 @@ def swap(x, y):
     except ValueError:
         print("Ошибка, переменые должны быть одинакового типа")
         return x, y
-a = int(input("Введите число"))
-b = int(input("Введите число"))
-c = int(input("Введите число"))
-d = int(input("Введите число"))
 
 try:
+    a = int(input("Введите число"))
+    b = int(input("Введите число"))
+    c = int(input("Введите число"))
+    d = int(input("Введите число"))
     a, b = swap(a,b)
     c, d = swap (c, d)
     b, c = swap (b, c)
