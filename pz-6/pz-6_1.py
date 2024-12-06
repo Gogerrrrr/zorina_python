6bbcf5b9b59bb85a12a794334c7c62cb606f1cb1
Gogerrrrr <zorinadarya682@gmail.com>
2024-12-06 10:38:14 +0300

    pz-6

---
+++ pz-6/pz-6_1.py
@@ -0,0 +1,25 @@
+#Дан целочисленный список размера 10. Вывести вначале
+#Содержащиеся в данном списке четные числа в порядке возрастания,
+#а затем - все нечетные числа в порядке убывания
+
+def sort_chet_nechet_numbers(numbers): #сортируем четные и нечетные числа
+    chet_numbers = []
+    nechet_numbers = []
+
+    for number in numbers:
+        if number % 2 == 0:
+            chet_numbers.append(number)
+        else:
+            nechet_numbers.append(number)
+    return chet_numbers, nechet_numbers
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+chet_numbers, nechet_numbers = sort_chet_nechet_numbers(numbers)
+
+print("Четные числа в порядке возрастания: ")
+for number in chet_numbers:
+    print(number)
+for number in reversed(nechet_numbers):
+    print(number)
