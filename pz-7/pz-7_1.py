8b6185ede39177ea1f37ff992f6760da8d90a573
Gogerrrrr <zorinadarya682@gmail.com>
2025-01-29 22:41:32 +0300

    pz-7_1.py

--- pz-7/pz-7_1.py
+++ pz-7/pz-7_1.py
@@ -5,8 +5,5 @@ text = input("Введите текст:")
 first_symbol = text[0]
 last_symbol =  text[-1]
 
-first_symbol_code = ord(first_symbol)
-last_symbol_code = ord(last_symbol)
-
 print(f"Код первого символа: {ord(text[0])}")
 print(f"Код последнего символа: {ord(text[-1])}")
