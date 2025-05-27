a6899a9452527563537cf7a53da00f4be8e4de23
Gogerrrrr <zorinadarya682@gmail.com>
2025-05-27 23:58:22 +0300

    pz-13_1.py

--- pz-13/pz-13_1.py
+++ pz-13/pz-13_1.py
@@ -1,16 +1,15 @@
+#В двумерном списке элементы второго стобца заменить элементами из
+#одномерного динамического массива соответствующей размерности
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
+new_column = [1, 114, 534]
+def replace_second_column(matrix, new_column):
+    return list(map(lambda x, zamena: x[:1] + [zamena] + x[2:], matrix, new_column))
 
-n = int(input("Введите номер столбца (от 0): "))
-
-new_matrix = list(map(
-    lambda row: [val * 2 if i == n else val for i, val in enumerate(row)],
-    matrix
-))
-
-print("Результат:")
-for row in new_matrix:
-    print(row)
+result = replace_second_column(matrix, new_column)
+print("Результат замены второго столбца:")
+for x in result:
+    print(x)
