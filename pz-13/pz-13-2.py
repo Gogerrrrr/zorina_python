fc083fc458a1a2f7e1a1c08cd5cfe229ba4ecd1e
Gogerrrrr <zorinadarya682@gmail.com>
2025-06-05 11:14:30 +0300

    Update pz-13-2.py

--- pz-13/pz-13-2.py
+++ pz-13/pz-13-2.py
@@ -1,15 +1,19 @@
 #в двумерном списке найти среднее арифметическое положительных элементов, кратных 3
+import random
+rows = 3
+columns = 3
+matrix = []
+for i in range(rows):
+    row = [random.randint(1, 100) for i in range(columns)]
+    matrix.append(row)
+print("Исходная матрица:")
+for row in matrix:
+    print(row)
 
 def kratnie_3(matrix):
     all_elements = [elem for krat in matrix for elem in krat]
     filtered = list(filter(lambda x: x > 0 and x % 3 == 0, all_elements))
     return sum(filtered) / len(filtered)
-
-matrix = [
-    [33, 2, 3],
-    [4, 6, -9],
-    [7, 9, 12]
-]
 
 result = kratnie_3(matrix)
 print("Среднее арифметическое положительных элементов, кратных 3:", result)
